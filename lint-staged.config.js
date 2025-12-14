f3f40ab1e0933b15ed96522beb6a6d7998d72c95
liveedevteam <livee.dev@gmail.com>
2025-12-14 20:57:23 +0700

    feat: enhance lint-staged configuration to include prettier and type-checking

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,7 @@
 /** @type {import('lint-staged').Config} */
 module.exports = {
   // Lint & format TypeScript and JavaScript files
-  "*.{js,jsx,ts,tsx}": ["eslint --fix"],
+  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write", () => "npm run type-check"],
   // Format JSON, MD, and config files
   "*.{json,md,yml,yaml}": ["prettier --write"],
 };
-
